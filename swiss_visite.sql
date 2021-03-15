c6163b048a58efb9edd23608e6bc066673974035
antony6666 <77482690+antony6666@users.noreply.github.com>
2021-03-15 14:49:28 +0100

    Add files via upload

---
+++ swiss_visite.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 15 mars 2021 à 13:47
+-- Version du serveur :  5.7.21
+-- Version de PHP :  7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `swiss_visite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `activite_compl`
+--
+
+DROP TABLE IF EXISTS `activite_compl`;
+CREATE TABLE IF NOT EXISTS `activite_compl` (
+  `AC_NUM` int(11) NOT NULL,
+  `AC_DATE` datetime DEFAULT NULL,
+  `AC_LIEU` varchar(25) DEFAULT NULL,
+  `AC_THEME` varchar(10) DEFAULT NULL,
+  `AC_MOTIF` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composant`
+--
+
+DROP TABLE IF EXISTS `composant`;
+CREATE TABLE IF NOT EXISTS `composant` (
+  `CMP_CODE` varchar(4) NOT NULL,
+  `CMP_LIBELLE` varchar(25) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `constituer`
+--
+
+DROP TABLE IF EXISTS `constituer`;
+CREATE TABLE IF NOT EXISTS `constituer` (
+  `MED_DEPOTLEGAL` varchar(10) NOT NULL,
+  `CMP_CODE` varchar(4) NOT NULL,
+  `CST_QTE` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dosage`
+--
+
+DROP TABLE IF EXISTS `dosage`;
+CREATE TABLE IF NOT EXISTS `dosage` (
+  `DOS_CODE` varchar(10) NOT NULL,
+  `DOS_QUANTITE` varchar(10) DEFAULT NULL,
+  `DOS_UNITE` varchar(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `famille`
+--
+
+DROP TABLE IF EXISTS `famille`;
+CREATE TABLE IF NOT EXISTS `famille` (
+  `FAM_CODE` varchar(3) NOT NULL,
+  `FAM_LIBELLE` varchar(80) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formuler`
+--
+
+DROP TABLE IF EXISTS `formuler`;
+CREATE TABLE IF NOT EXISTS `formuler` (
+  `MED_DEPOTLEGAL` varchar(10) NOT NULL,
+  `PRE_CODE` varchar(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `interagir`
+--
+
+DROP TABLE IF EXISTS `interagir`;
+CREATE TABLE IF NOT EXISTS `interagir` (
+  `MED_PERTURBATEUR` varchar(10) NOT NULL,
+  `MED_MED_PERTURBE` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inviter`
+--
+
+DROP TABLE IF EXISTS `inviter`;
+CREATE TABLE IF NOT EXISTS `inviter` (
+  `AC_NUM` int(11) NOT NULL,
+  `PRA_NUM` int(11) NOT NULL,
+  `SPECIALISTEON` tinyint(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `labo`
+--
+
+DROP TABLE IF EXISTS `labo`;
+CREATE TABLE IF NOT EXISTS `labo` (
+  `LAB_CODE` varchar(2) NOT NULL,
+  `LAB_NOM` varchar(10) DEFAULT NULL,
+  `LAB_CHEFVENTE` varchar(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `medicament`
+--
+
+DROP TABLE IF EXISTS `medicament`;
+CREATE TABLE IF NOT EXISTS `medicament` (
+  `MED_DEPOTLEGAL` varchar(10) NOT NULL,
+  `MED_NOMCOMMERCIAL` varchar(25) DEFAULT NULL,
+  `FAM_CODE` varchar(3) DEFAULT NULL,
+  `MED_COMPOSITION` varchar(255) DEFAULT NULL,
+  `MED_EFFETS` varchar(255) DEFAULT NULL,
+  `MED_CONTREINDIC` varchar(255) DEFAULT NULL,
+  `MED_PRIXECHANTILLON` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offrir`
+--
+
+DROP TABLE IF EXISTS `offrir`;
+CREATE TABLE IF NOT EXISTS `offrir` (
+  `VIS_MATRICULE` varchar(10) NOT NULL,
+  `RAP_NUM` int(11) NOT NULL,
+  `MED_DEPOTLEGAL` varchar(10) NOT NULL,
+  `OFF_QTE` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posseder`
+--
+
+DROP TABLE IF EXISTS `posseder`;
+CREATE TABLE IF NOT EXISTS `posseder` (
+  `PRA_NUM` int(11) NOT NULL,
+  `SPE_CODE` varchar(5) NOT NULL,
+  `POS_DIPLOME` varchar(10) DEFAULT NULL,
+  `POS_COEFPRESCRIPTION` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `praticien`
+--
+
+DROP TABLE IF EXISTS `praticien`;
+CREATE TABLE IF NOT EXISTS `praticien` (
+  `PRA_NUM` int(11) NOT NULL,
+  `PRA_NOM` varchar(25) DEFAULT NULL,
+  `PRA_PRENOM` varchar(30) DEFAULT NULL,
+  `PRA_ADRESSE` varchar(50) DEFAULT NULL,
+  `PRA_CP` varchar(5) DEFAULT NULL,
+  `PRA_VILLE` varchar(25) DEFAULT NULL,
+  `PRA_COEFNOTORIETE` double DEFAULT NULL,
+  `TYP_CODE` varchar(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
